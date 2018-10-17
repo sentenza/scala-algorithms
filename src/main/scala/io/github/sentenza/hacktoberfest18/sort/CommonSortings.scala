83208eb6ce0d1a998f6dfb740b64f9d44a76bf35
Grischa Wende <lokke@lokke.me>
2018-10-17 20:35:55 +0200

    Refactoring, added test for bubblesort

--- src/main/scala/io/github/sentenza/hacktoberfest18/sort/CommonSortings.scala
+++ src/main/scala/io/github/sentenza/hacktoberfest18/sort/CommonSortings.scala
@@ -180,19 +180,16 @@ object CommonSortings {
     def bubbleSort1(array: Array[Int]): Array[Int] = {
 
         def go(a: Array[Int], i: Int): Array[Int] = {
-            if(i+1 >= a.length) a
-            else if(a(i) > a(i+1)) go(a.patch(i, Array(a(i+1), a(i)), 2), 0)
-            else go(a, i+1)
+          (a, i) match {
+            case (a, i) if i+1 >= a.length => a
+            case (a, i) if(a(i) > a(i+1)) => go(a.patch(i, Array(a(i+1), a(i)), 2), 0)
+            case (a, i) => go(a, i+1)
+          }
         }
 
         go(array, 0)
-        }
+    }
 
-
-    def selectionSort(array: Array[Int]): Array[Int] = ???
-    def insertionSort(array: Array[Int]): Array[Int] = ???
-    def quickSort(array: Array[Int]): Array[Int] = ???
-    def mergeSort(array: Array[Int]): Array[Int] = ???
   }
 }
 
