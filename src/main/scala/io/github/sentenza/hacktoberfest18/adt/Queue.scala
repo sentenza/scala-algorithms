356882e5429755c4a52d2553e800c473f710a5d4
Alfredo <alfredotorre@gmail.com>
2018-10-27 19:54:16 +0100

    chore: adding License header

--- src/main/scala/io/github/sentenza/hacktoberfest18/adt/Queue.scala
+++ src/main/scala/io/github/sentenza/hacktoberfest18/adt/Queue.scala
@@ -1,5 +1,20 @@
 package io.github.sentenza.hacktoberfest18.adt
 
+/*
+ * HacktoberFest 2018 - Scala Algorhitms
+ * Copyright (C) 2018 sentenza
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /** Functional Queue
   *
   * A *fully persistent* data structure with three operations:
