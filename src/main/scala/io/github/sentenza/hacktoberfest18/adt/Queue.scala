26776746631816236b160ee78f1d1da9f8c844c4
Alfredo <alfredotorre@gmail.com>
2018-11-02 12:38:18 +0000

    feat: add a fully persistent and functional queue implementation

--- src/main/scala/io/github/sentenza/hacktoberfest18/adt/Queue.scala
+++ src/main/scala/io/github/sentenza/hacktoberfest18/adt/Queue.scala
@@ -16,13 +16,17 @@ package io.github.sentenza.hacktoberfest18.adt
  */
 
 /** Functional Queue
+  * Defines the generic type {{{ Queue[T] }}}
   *
+  * @see Programming in Scala (2nd edition) Ch. 19.7
+  * Note that Queue is covariant in T
+  * [[https://sentenza.github.io/docs/scala/#variance Read about variance]]
   * A *fully persistent* data structure with three operations:
   * - head: returns the first element of the queue
   * - tail: returns a queue without the first element
   * - enqueue: returns a NEW queue with a given element appended to the end
   */
-trait Queue[T] {
+trait Queue[+T] {
 
   /** @return The first element of the queue */
   def head: T
@@ -33,7 +37,96 @@ trait Queue[T] {
   /**
     * Appends and element x to the end of the queue
     * @param x The element to be appended to the end
+    * @tparam U We need to define T as the LOWER BOUND of U, because the type of
+    *           the function parameters must be in contravariant position
     * @return A new queue containing x at its end
     */
-  def enqueue(x: T): Queue[T]
+  def enqueue[U >: T](x: U): Queue[U]
+
+  /**
+    * @return True if the queue is empty
+    */
+  def isEmpty: Boolean
+
+  /**
+    * !Queue.isEmtpy()
+    */
+  def nonEmpty: Boolean
+
+  override def toString(): String
+}
+
+/**
+  * The companion object Queue.
+  * @example {{{ Queue(13, 21, 34, 55, 89, 144) }}}
+  */
+object Queue {
+
+  /** Factory method */
+  def apply[T](xs: T*): Queue[T] =
+    new QueueImpl[T](xs.toList, Nil)
+
+  /**
+    * This queue is still a pure functional object, even though we're
+    * using INTERNALLY reassignable immutable fields
+    * @param leading The start fo the queue
+    * @param trailing The end of the queue
+    */
+  private class QueueImpl[+T](
+      private[this] var leading: List[T],
+      private[this] var trailing: List[T]
+  ) extends Queue[T] {
+
+    /**
+      * This is an impure function but we need it in order to minimise the
+      * complexity of reversing an immutable list and then copying across
+      * all the elements to have at each moment:
+      * queue = leading :: (trailing.reverse)
+      */
+    private def mirror(): Unit =
+      if (leading.isEmpty) {
+        while (trailing.nonEmpty) {
+          // Reversing
+          leading = trailing.head :: leading
+          trailing = trailing.tail
+        }
+      }
+
+    def head: T = {
+      mirror() // Internal side effect
+      leading.head
+    }
+
+    def tail: QueueImpl[T] = {
+      mirror() // Internal side effect
+      new QueueImpl(leading.tail, trailing)
+    }
+
+    // NOTE: We're adding the new element x to head of the trailing List
+    def enqueue[U >: T](x: U): Queue[U] =
+      new QueueImpl[U](leading, x :: trailing)
+
+    def nonEmpty: Boolean = {
+      leading.nonEmpty || trailing.nonEmpty
+    }
+
+    def isEmpty: Boolean =
+      leading.isEmpty && trailing.isEmpty
+
+    /**
+      * @param separator The String separator (e.g. ",", ":"
+      * @param l The leading list
+      * @param t The trailing list
+      */
+    private def buildPrint[U >: T](separator: String)(l: List[U])(t: List[U]): String = t match {
+      case Nil => l.mkString("Queue(", separator, ")")
+      case _   => l.mkString("Queue(", separator, separator) + t.mkString("", separator, ")")
+
+    }
+
+    override def toString(): String = {
+      buildPrint(", ")(leading)(trailing)
+    }
+  }
+
 }
