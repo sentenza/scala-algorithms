b654834552b9e5462be5ead805e34735d9defa3d
Ivano Pagano <pagoda_5b@hotmail.com>
2021-10-16 13:54:11 +0200

    feat: Restore a working interactive menu (#67)
    * feat: Restore a working interactive menu
    
    * refactor: use scala.reflect to call sorts methods
    
    * Use scala reflection which gives more specific types for
      the dynamically invoked sort methods
    * Clean up and address review comments
    * Fix a small bug when navigating back from the sort sub-menu
    
    * feat: group all reflective code
    
     * Put reflection in a single method which collects and check the
       available sort methods' signature type, to directly produce a
       more regular function to use in the menu-entry
     * Fix the formatting in the build file

--- src/main/scala/io/github/sentenza/hacktoberfest/MenuIO.scala
+++ src/main/scala/io/github/sentenza/hacktoberfest/MenuIO.scala
@@ -3,6 +3,11 @@ package io.github.sentenza.hacktoberfest
 import System.out.println
 import scala.annotation.tailrec
 import scala.util.{Try, Success}
+import io.github.sentenza.hacktoberfest.algos.sort.MutableSorting
+import io.github.sentenza.hacktoberfest.algos.sort.ImmutableSorting
+import io.github.sentenza.hacktoberfest.algos.sort.Sorting
+import scala.reflect.runtime.universe._
+import scala.reflect.ClassTag
 
 /*
  * HacktoberFest - Scala Algorithms
@@ -43,29 +48,80 @@ object MenuIO {
     */
   def printDisclaimer(): Unit = { println(heading + gplDisclaimer) }
 
-  private val noOp = () => ()
+  def readNumberInputs(): Array[Int] = {
+    print("Please enter a list of comma separated integers: ")
+    scala.io.StdIn.readLine().split(",").map(_.trim.toInt)
+  }
 
-  def readNumberInputs: Array[Int] = scala.io.StdIn.readLine().split(",").map(_.toInt)
+  case class MenuEntry(selector: Int, display: String, code: () => Unit = () => ())
 
-  case class MenuEntry(selector: Int, display: String, code: () => Unit)
+  /* The following section generalize how to display collections as
+   * strings, needed to interact with user input on the console
+   */
+  trait Show[T] {
+    def toString(t: T): String
+  }
+
+  implicit def showArray[T]: Show[Array[T]] = new Show[Array[T]] {
+    def toString(t: Array[T]): String = t.mkString(",")
+  }
+
+  implicit def showIterable[T, F[_] <: Iterable[_]]: Show[F[T]] = new Show[F[T]] {
+    def toString(t: F[T]): String = t.mkString(",")
+  }
 
   // TODO: Add more categories here
-  private val entries =
+  private val rootEntries =
     List(
-      MenuEntry(1, "Sorting algorithms", () => println("You chose sorting\n")),
-      MenuEntry(0, "Quit the program", noOp)
+      MenuEntry(
+        1,
+        "Sorting algorithms",
+        () => {
+          println("You chose sorting\n")
+          renderInteractiveMenu(sortingMenuEntries)
+        }
+      ),
+      MenuEntry(0, "Quit the program", () => System.exit(0))
     )
 
+  private lazy val sortingMenuEntries = List(
+    MenuEntry(
+      2,
+      "MutableSorting",
+      () => {
+        println("You chose mutable sorting.")
+        renderInteractiveMenu(
+          createSortMenuEntries[Array, Int, MutableSorting.type](
+            MutableSorting,
+            readNumberInputs()
+          )
+        )
+      }
+    ),
+    MenuEntry(
+      1,
+      "ImmutableSorting",
+      () => {
+        println("You chose immutable sorting.")
+        renderInteractiveMenu(
+          createSortMenuEntries[List, Int, ImmutableSorting.type](
+            ImmutableSorting,
+            readNumberInputs().toList
+          )
+        )
+      }
+    ),
+    MenuEntry(0, "Quit sorting", () => renderInteractiveMenu())
+  )
+
   @tailrec
-  def renderInteractiveMenu(): Unit = {
+  def renderInteractiveMenu(entries: List[MenuEntry] = rootEntries): Unit = {
     println("Please choose:")
-    entries.foreach { case MenuEntry(num, label, _) =>
+    entries.sortBy(_.selector).foreach { case MenuEntry(num, label, _) =>
       println(s"$num: $label")
     }
 
     Try(scala.io.StdIn.readInt()) match {
-      case Success(0) =>
-        ()
       case Success(choice) if entries.exists(_.selector == choice) =>
         entries.find(_.selector == choice).foreach { case MenuEntry(_, _, code) =>
           code()
@@ -76,4 +132,53 @@ object MenuIO {
         renderInteractiveMenu()
     }
   }
+
+  private def createSortMenuEntries[F[_], T, S <: Sorting[F, T]: TypeTag: ClassTag](
+      sorting: S,
+      toSort: => F[T]
+  )(implicit show: Show[F[T]], ftt: TypeTag[F[T]]) = {
+
+    val (_, entries) = collectSortMethods[S, F[T]](sorting)
+      .foldLeft(1 -> List.empty[MenuEntry]) { case ((count, entries), (sortName, sortFunction)) =>
+        count + 1 -> (entries :+ MenuEntry(
+          count,
+          sortName,
+          () => executeSort(sortName, sortFunction, toSort)
+        ))
+      }
+
+    entries
+  }
+
+  private def executeSort[T: Show](name: String, sorter: T => T, toSort: => T) = {
+    val show = implicitly[Show[T]]
+
+    println(s"You've chosen $name!")
+    // forces the on-demand evaluation of the input collection
+    val values = toSort
+    println(
+      s"You entered:${show.toString(values)}. They are going to be sorted by $name.\n Sorting..."
+    )
+    println(s"Your number entries sorted are: ${show.toString(sorter(values))}")
+  }
+
+  private def collectSortMethods[C: TypeTag: ClassTag, T: TypeTag](c: C) =
+    typeOf[C].decls.collect {
+      case t
+          if
+          // matches all sort methods that takes exactly an T (some collection type) and spits a T again
+          t.fullName.endsWith("Sort") &&
+            t.isMethod &&
+            t.asMethod.paramLists
+              .exists(ins => ins.headOption.exists(_.info =:= typeOf[T])) &&
+            t.asMethod.returnType =:= typeOf[T] =>
+        // we get a method handle and wrap it in a typed function to return back
+        val method =
+          runtimeMirror(c.getClass().getClassLoader())
+            .reflect(c)
+            .reflectMethod(t.asMethod)
+        // use the method name as key and the function as value of this map
+        t.name.decodedName.toString() -> { (in: T) => method(in).asInstanceOf[T] }
+    }.toMap
+
 }
